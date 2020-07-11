1f9ddcd5babd888c2456977bcd99969bae5cbbba
Kwatanwa17 <kwatanwa17@gmail.com>
2020-07-11 18:20:54 +0900

    bracket pass

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -14,17 +14,67 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i))
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            # Process closing bracket, write your code her
+            if len(opening_brackets_stack) == 0:
+                return i + 1
+
+            top = opening_brackets_stack[-1]
+
+            if are_matching(top[0], next):
+                opening_brackets_stack.pop(-1)
+            else:
+                return i + 1
+
+    if len(opening_brackets_stack) != 0:
+        return opening_brackets_stack[-1][1] + 1
+    else:
+        return "Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
+    # # Printing answer, write your code here
+    # import os
+    #
+    # filename_list = []
+    #
+    # for root, dirs, files in os.walk("./tests"):
+    #
+    #     for filename in files:
+    #         if '.a' not in filename:
+    #             filename_list.append(filename)
+    #             filename_list.sort()
+    #
+    #     for filename in filename_list:
+    #         print('........................')
+    #         print('Running Test: ' + filename)
+    #         f = open("./tests/" + filename, "r")
+    #
+    #         if f.mode == 'r':
+    #             input = f.read().rstrip('\n')
+    #             f.close()
+    #             f = open("./tests/" + filename + '.a', "r")
+    #
+    #         if f.mode == 'r':
+    #             expected_output = f.read().rstrip('\n')
+    #             f.close()
+    #
+    #         output = find_mismatch(input)
+    #
+    #         print('Input: ' + str(input))
+    #         print('Expected Output: ' + str(expected_output))
+    #         print('Ouput: ' + str(output))
+    #
+    #     if str(expected_output) == str(output):
+    #         print('Test Passed')
+    #     else:
+    #         print('Test Failed')
+    #         break
 
 
 if __name__ == "__main__":
