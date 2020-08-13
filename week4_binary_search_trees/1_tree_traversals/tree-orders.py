e23aa8194d36e3d493dad0d0e0a65eb1a2a74b7c
Kwatanwa17 <kwatanwa17@gmail.com>
2020-08-13 18:30:34 +0900

    preorder test passed

--- week4_binary_search_trees/1_tree_traversals/tree-orders.py
+++ week4_binary_search_trees/1_tree_traversals/tree-orders.py
@@ -45,7 +45,6 @@ class TreeOrders:
                 self.result.append(self.key[current])
             s.pop()
 
-
             # current belongs to the left subtree of the last element of the stack, which is its ancestor
             while s and (self.left[s[len(s) - 1]]) != current:
                 current = s[len(s) - 1]
@@ -69,6 +68,40 @@ class TreeOrders:
         self.result = []
         # Finish the implementation
         # You may need to add a new recursive method to do that
+        current = 0
+        # stack
+        s = [current]
+
+        while s:
+            if self.key[current] not in self.result and current != -1:
+                self.result.append(self.key[current])
+
+            # if the vertex has a left son
+            if self.left[current] != -1:
+                # append the left son index to the stack
+                s.append(self.left[current])
+                current = self.left[current]
+                # if self.left[current] == -1 the stack remains as it is
+                continue
+
+            s.pop()
+
+            # current belongs to the left subtree of the last element of the stack, which is its ancestor
+            while s and (self.left[s[len(s) - 1]]) != current:
+                current = s[len(s) - 1]
+                # if current != -1:
+                #     self.result.append(self.key[current])
+                s.pop()
+
+            if s:
+                s.append(self.right[current])
+                current = self.right[current]
+                # if current != -1:
+                #     self.result.append(self.key[current])
+            # go to the right subtree
+            elif len(self.result) < len(self.key):
+                s.append(self.right[current])
+                current = self.right[current]
 
         return self.result
 
@@ -76,6 +109,47 @@ class TreeOrders:
         self.result = []
         # Finish the implementation
         # You may need to add a new recursive method to do that
+        current = 0
+        # stack
+        s = [current]
+
+        while s:
+
+            # if the vertex has a left son
+            if self.left[current] != -1:
+                # append the left son index to the stack
+                s.append(self.left[current])
+                current = self.left[current]
+                # if self.left[current] == -1 the stack remains as it is
+                continue
+            if self.left[current] == -1 and self.right[current] == -1:
+                s.pop()
+
+            if self.key[current] not in self.result and current != -1:
+                self.result.append(self.key[current])
+
+            # current belongs to the left subtree of the last element of the stack, which is its ancestor
+            while s and (self.left[s[len(s) - 1]]) != current:
+                current = s[len(s) - 1]
+                # if current != -1:
+                #     self.result.append(self.key[current])
+                if self.left[current] == -1 and self.right[current] == -1:
+                    s.pop()
+                else:
+                    break
+
+            if s:
+                if self.right[current] not in s:
+                    if self.key[self.right[current]] not in self.result:
+                        s.append(self.right[current])
+                        current = self.right[current]
+                # if current != -1:
+                #     self.result.append(self.key[current])
+            # go to the right subtree
+            elif len(self.result) < len(self.key):
+                s.append(self.right[current])
+                current = self.right[current]
+
 
         return self.result
 
