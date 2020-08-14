51c15e9fcb5f00a5828e11e845f0f4f3b0c739ca
Kwatanwa17 <kwatanwa17@gmail.com>
2020-08-14 13:40:42 +0900

    is bts advanced passed

--- week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
+++ week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
@@ -2,22 +2,86 @@
 
 import sys, threading
 
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+threading.stack_size(2 ** 27)  # new thread will get stack of such size
+
 
 def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+    # set exit value
+    exit_value = True
+    if len(tree) > 0:
+
+        # key
+        key = [i[0] for i in tree]
+
+        # left
+        left = [i[1] for i in tree]
+
+        # right
+        right = [i[2] for i in tree]
+
+        current = 0
+        result = []
+        result_idx = []
+
+        # stack
+        s = []
+
+        # set exit value
+        exit_value = True
+
+        # subtree 0 = left, 1 = right
+        # subtree = 0
+
+        while exit_value:
+            if current != -1:
+                s.append(current)
+                current = left[current]
+
+            elif s:
+                current = s.pop()
+
+                # check current node sons
+                if left[current] != -1:
+                    if key[left[current]] < key[current]:
+                        pass
+                    else:
+                        exit_value = False
+
+                if right[current] != -1:
+                    if key[right[current]] >= key[current]:
+                        pass
+                    else:
+                        exit_value = False
+
+                result.append(key[current])
+
+                current = right[current]
+
+            else:
+                break
+
+        # find index of the root node
+        # root_idx = result_idx.index(0)
+
+        if sorted(result) != result:
+            exit_value = False
+
+    return exit_value
 
 
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
-    print("CORRECT")
-  else:
-    print("INCORRECT")
+    # f = open("./tests/4")
+    # nodes = int(f.readline().strip())
+    nodes = int(sys.stdin.readline().strip())
+    tree = []
+    for i in range(nodes):
+        tree.append(list(map(int, sys.stdin.readline().strip().split())))
+        # tree.append(list(map(int, f.readline().strip().split())))
+    if IsBinarySearchTree(tree):
+        print("CORRECT")
+    else:
+        print("INCORRECT")
+
 
 threading.Thread(target=main).start()
